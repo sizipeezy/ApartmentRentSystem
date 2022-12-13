1375a0a28156b8bf06e294dd54fb55ed4aadaf0c
Sezen <sezen.ibrahim3@gmail.com>
2022-12-13 18:22:12 +0200

    Introduced ApartmentsController and Actions

---
+++ ApartmentRentSystem.Core/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ApartmentRentSystem.Core.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class IndexViewModel
+    {
+    }
+}
---
+++ ApartmentRentSystem/Controllers/ApartmentsController.cs
@@ -0,0 +1,96 @@
+﻿namespace ApartmentRentSystem.Controllers
+{
+    using ApartmentRentSystem.Core.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ApartmentsController : Controller
+    {
+        public IActionResult Index() => View(new IndexViewModel());
+
+        public IActionResult All()
+        {
+            return this.View();
+        }
+
+        public IActionResult Mine()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddApartmentModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return this.View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Details(int id, ApartmentDetailsModel model)
+        {
+            //Iguard check for the Id
+
+            if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(All));
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return this.View(new ApartmentEditModel());   
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ApartmentEditModel model)
+        {
+            return this.RedirectToAction(nameof(Details), new { id = id });
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return this.View(new ApartmentDetailsModel());
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Delete(ApartmentDetailsModel model)
+        {
+            return this.RedirectToAction(nameof(All));
+        }
+
+        [HttpPost]
+        public  IActionResult Rent(int id)
+        {
+            return this.RedirectToAction(nameof(Mine));
+        }
+
+        [HttpPost]
+        public IActionResult Leave(int id)
+        {
+            return this.RedirectToAction(nameof(Mine));
+        }
+    }
+}
