0a8fa490e5ccc2fe7406d6e8a42c5ed48f5c309a
Sezen <sezen.ibrahim3@gmail.com>
2022-12-16 22:29:24 +0200

    Fixed bug with view

--- ApartmentRentSystem.Core/Models/IndexViewModel.cs
+++ ApartmentRentSystem.Core/Models/IndexViewModel.cs
@@ -1,12 +1,5 @@
 ﻿namespace ApartmentRentSystem.Core.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-
     public class IndexViewModel
     {
         public int Id { get; set; }
