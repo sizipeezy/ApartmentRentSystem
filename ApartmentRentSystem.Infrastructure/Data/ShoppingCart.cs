93a3d4f67814f802fb4f470f5f6600bf40a48b7a
Sezen <sezen.ibrahim3@gmail.com>
2022-12-24 13:16:20 +0200

    Added ViewComponent for View

--- ApartmentRentSystem.Infrastructure/Data/ShoppingCart.cs
+++ ApartmentRentSystem.Infrastructure/Data/ShoppingCart.cs
@@ -1,6 +1,7 @@
 ﻿namespace ApartmentRentSystem.Infrastructure.Data
 {
     using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
     using System.Collections.Generic;
 
@@ -29,6 +30,44 @@
             session.SetString("CartId", cartId);
 
             return new ShoppingCart(context) { ShoppingCartId = cartId};   
+        }
+
+        public List<Item> GetItems() => 
+                this.data
+                .Items
+                .Where(x => x.ShoppingCartId == ShoppingCartId)
+                .Include(x => x.Apartment)
+                .ToList();
+
+        public double Total() =>
+            (double)this.data.Items
+            .Where(x => x.ShoppingCartId == ShoppingCartId)
+            .Select(x => x.Apartment.PricePerMonth * x.Quantity)
+            .Sum();
+
+        public void Add(int id)
+        {
+            var item = this.data.Items.FirstOrDefault(x => x.Id == id && x.ShoppingCartId == ShoppingCartId);
+
+            var apartment = this.data.Apartments.FirstOrDefault(x => x.Id == id);
+
+            if(item == null)
+            {
+                item = new Item
+                {
+                    Quantity = 1,
+                    Apartment = apartment,
+                    ShoppingCartId = ShoppingCartId
+                };
+
+                data.Items.Add(item);
+            }
+            else
+            {
+                item.Quantity++;
+            }
+
+            data.SaveChanges();
         }
     }
 }
---
+++ ApartmentRentSystem.Infrastructure/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿namespace ApartmentRentSystem.Infrastructure.ViewComponents
+{
+    using ApartmentRentSystem.Infrastructure.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart cart;
+
+        public ShoppingCartSummary(ShoppingCart cart)
+        {
+            this.cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = this.cart.GetItems();
+
+            return this.View(items.Count);
+        }
+    }
+}
