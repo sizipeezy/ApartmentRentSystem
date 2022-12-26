22fe1165ae94946288ba8ec3ccb80871335c7c9d
Sezen <sezen.ibrahim3@gmail.com>
2022-12-26 20:16:14 +0200

    Setting the Tests Project

---
+++ ApartmentRentSystem.Tests/DatabaseMock.cs
@@ -0,0 +1,21 @@
+﻿namespace ApartmentRentSystem.Tests
+{
+    using ApartmentRentSystem.Infrastructure.Data;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public class DatabaseMock
+    {
+        public static ApplicationDbContext Instance
+        {
+            get
+            {
+                var dbcontextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("ApartmentRentSystem " + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new ApplicationDbContext(dbcontextOptions);
+            }
+        }
+    }
+}
---
+++ ApartmentRentSystem.Tests/UnitTests/Base.cs
@@ -0,0 +1,57 @@
+﻿using ApartmentRentSystem.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentRentSystem.Tests.UnitTests
+{
+    public class Base
+    {
+        protected ApplicationDbContext data;
+
+        [OneTimeSetUp]
+
+        public void SetUpBase()
+        {
+            this.data = DatabaseMock.Instance;
+            this.SeedDatabase();
+        }
+
+        public ApplicationUser Renter { get; set; } = null!;
+
+        public Agent Agent { get; set; }
+
+        public Apartment RentedApartment { get; set; }
+
+        private void SeedDatabase()
+        {
+            this.Renter = new ApplicationUser()
+            {
+                Id= "RenterUserId",
+                Email = "rent@er.bg",
+                FirstName = "Renter",
+                LastName = "User"
+            };
+
+            this.data.Users.Add(this.Renter);
+
+            var agent = new Agent()
+            {
+                PhoneNumber = "08985748212",
+                User = new ApplicationUser()
+                {
+                    Id = "TestUserId",
+                    Email = "TestUser@test.bg",
+                    FirstName = "Test",
+                    LastName = "Testov"
+                }
+            };
+
+            this.data.Agents.Add(this.Agent);
+        }
+
+
+    }
+}
