f4a38c60a1089a4407804f6d03f1ee8ea289c41d
Sezen <sezen.ibrahim3@gmail.com>
2023-01-01 23:01:58 +0200

    UserService Tests

---
+++ ApartmentRentSystem.Tests/UnitTests/UserServiceTests.cs
@@ -0,0 +1,54 @@
+﻿namespace ApartmentRentSystem.Tests.UnitTests
+{
+    using ApartmentRentSystem.Core.Contracts;
+    using ApartmentRentSystem.Core.Services;
+    using ApartmentRentSystem.Infrastructure.Data;
+    using AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+
+
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private ApplicationDbContext data;
+        private IUserService userService;
+        private IMapper mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+           .UseInMemoryDatabase("ApartmentRentSystem")
+            .Options;
+
+            data = new ApplicationDbContext(contextOptions);
+
+            data.Database.EnsureDeleted();
+            data.Database.EnsureCreated();
+
+            this.mapper = MapperMock.Instance;
+        }
+
+        [Test]
+        public void GetUserNameWithNoUserName()
+        {
+            userService = new UserService(data, mapper);
+
+            var fakeUser = userService.GetUserName("dea12856-c198-4129-b3f3-b893d8395082");
+
+            Assert.IsTrue(fakeUser == null);
+        }
+
+        [Test]
+        public void GetUsersShouldSucceed()
+        {
+            userService = new UserService(data, mapper);
+
+            var fakeUser = userService.GetUsers();
+
+            Assert.IsTrue(fakeUser != null);
+            Assert.That(fakeUser.Any());
+        }
+    }
+}
