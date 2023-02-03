6b6a3d812ac6af2fe957c8a858e6b2a6587d0ea1
Sezen <sezen.ibrahim3@gmail.com>
2023-02-03 22:37:11 +0200

    Custom Extension Method TotalPrice

---
+++ ApartmentRentSystem/Extensions/GetTotalShoppingCartPriceExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace ApartmentRentSystem.Extensions
+{
+    using ApartmentRentSystem.Infrastructure.Data;
+
+
+    public static class GetTotalShoppingCartPriceExtension
+    {
+        public static decimal Total(this ShoppingCart cart)
+        {
+            decimal total = 0;
+            foreach (var item in cart.Items)
+            {
+                total += item.Apartment.PricePerMonth;
+            }
+
+            return total;
+        }
+    }
+}
